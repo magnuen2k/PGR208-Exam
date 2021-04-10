eed6c91b65750e39cd3ca18634bca4a26138f0a8
crashable <31343152+crashable@users.noreply.github.com>
2021-04-10 16:59:04 +0200

    Removed several components that wont be needed for the actual project.

--- app/src/main/java/no/kristiania/pgr208_exam/MainActivity.kt
+++ app/src/main/java/no/kristiania/pgr208_exam/MainActivity.kt
@@ -1,11 +1,20 @@
 package no.kristiania.pgr208_exam
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import no.kristiania.pgr208_exam.data.imageList.ImageListFragment
+import no.kristiania.pgr208_exam.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, ImageListFragment(), "comics_list").commit()
     }
 }
