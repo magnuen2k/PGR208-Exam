0376ef1a2c9fe0fcf7ec1e0df5cb5ff053f52e27
crashable <31343152+crashable@users.noreply.github.com>
2021-04-11 15:44:43 +0200

    Fixed colorformatting on CcOverviewAdapter.kt

--- app/src/main/java/no/kristiania/pgr208_exam/data/ccList/CcOverviewAdapter.kt
+++ app/src/main/java/no/kristiania/pgr208_exam/data/ccList/CcOverviewAdapter.kt
@@ -3,14 +3,18 @@ package no.kristiania.pgr208_exam.data.ccList
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import no.kristiania.pgr208_exam.R
 import no.kristiania.pgr208_exam.databinding.CcOverviewItemBinding
 import no.kristiania.pgr208_exam.data.domain.CcOverview
 import no.kristiania.pgr208_exam.data.domain.SpecificCcData
 import java.math.BigDecimal
 import java.text.DecimalFormat
-import java.text.NumberFormat
 import java.util.*
 
 
@@ -29,18 +33,32 @@ class CcOverviewAdapter(private val list: MutableList<SpecificCcData>, val onCli
 
     override fun getItemCount(): Int = list.size
 
-    class ImageViewHolder(private val binding: CcOverviewItemBinding, val onClick: (CcOverview) -> Unit) : RecyclerView.ViewHolder(binding.root){
-         fun bind(ccOverview: SpecificCcData){
+    class ImageViewHolder(private val binding: CcOverviewItemBinding, val onClick: (CcOverview) -> Unit) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(ccOverview: SpecificCcData) {
 
-             binding.ccId.text = ccOverview.id
-             binding.symbol.text = ccOverview.symbol
-             binding.currencyUsd.text = formatDecimal(ccOverview.priceUsd)
-             binding.changePercent.text = formatDecimal(ccOverview.changePercent24Hr)
-             Glide.with(binding.root.context).load("https://static.coincap.io/assets/icons/" + ccOverview.symbol?.toLowerCase(
-                 Locale.ROOT) + "@2x.png").into(binding.imgSmaller)
+            binding.ccId.text = ccOverview.id
+            binding.symbol.text = ccOverview.symbol
+            binding.currencyUsd.text = "$${formatDecimal(ccOverview.priceUsd)}"
+            binding.changePercent.text = "${formatPercent(ccOverview.changePercent24Hr, binding.changePercent)}%"
+            Glide.with(binding.root.context).load("https://static.coincap.io/assets/icons/" + ccOverview.symbol?.toLowerCase(
+                    Locale.ROOT) + "@2x.png").into(binding.imgSmaller)
         }
 
-        private fun formatDecimal(decimal: String?) : String {
+        private fun formatPercent(decimal: String?, textView: TextView): String {
+            val num = decimal?.toBigDecimal()?.signum()
+            if (num !== null) {
+                val color: Int =
+                        when {
+                            num > 0 -> R.color.positivePercent
+                            num < 0 -> R.color.negativePercent
+                            else -> R.color.neutralPercent
+                        }
+                textView.setTextColor(getColor(textView.context, color))
+            }
+            return formatDecimal(decimal)
+        }
+
+        private fun formatDecimal(decimal: String?): String {
             val priceUsd = decimal?.toBigDecimal()
             val format = DecimalFormat("#,###.00")
             format.isParseBigDecimal = true
