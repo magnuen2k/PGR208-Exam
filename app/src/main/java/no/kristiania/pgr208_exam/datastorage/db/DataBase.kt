97dd695fb363127bd720c72f33c22664d5dc2ee5
magnuen2k <maen030@student.kristiania.no>
2021-04-30 18:02:11 +0200

    add all transactions to transaction table in database

--- app/src/main/java/no/kristiania/pgr208_exam/datastorage/db/DataBase.kt
+++ app/src/main/java/no/kristiania/pgr208_exam/datastorage/db/DataBase.kt
@@ -5,14 +5,15 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import no.kristiania.pgr208_exam.datastorage.entities.UserPortfolio
+import no.kristiania.pgr208_exam.datastorage.entities.UserTransaction
 
 
 const val DATABASE_NAME: String = "accounting_database"
 
-@Database(entities = [UserPortfolio::class], version = 1)
+@Database(entities = [UserPortfolio::class, UserTransaction::class], version = 1)
 abstract class DataBase : RoomDatabase() {
     abstract fun getUserPortfolioDAO() : UserPortfolioDAO
-    //abstract fun getUserTransactionsDAO() : UserTransactionsDAO
+    abstract fun getUserTransactionsDAO() : UserTransactionsDAO
     companion object {
         private var db: DataBase? = null
 
--- app/src/main/java/no/kristiania/pgr208_exam/viewmodels/TransactionViewModel.kt
+++ app/src/main/java/no/kristiania/pgr208_exam/viewmodels/TransactionViewModel.kt
@@ -52,7 +52,7 @@ class TransactionViewModel(application: Application) : AndroidViewModel(applicat
         viewModelScope.launch {
             DataBase.getDatabase(getApplication()).getUserPortfolioDAO().insertPortfolio(symbol, volume)
             // Also add to transaction table (should give timestamp as well
-            //DataBase.getDatabase(getApplication()).getUserTransactionsDAO().insert(UserTransaction(symbol, volume))
+            DataBase.getDatabase(getApplication()).getUserTransactionsDAO().insert(UserTransaction(symbol, volume))
         }
     }
 
