c67b57aea2a493f869723cbb2a03eff674d6559e
crashable <31343152+crashable@users.noreply.github.com>
2021-05-02 20:05:26 +0200

    UserPortfolioFragment.kt / "My portfolio page" now displays the value of ones investments

--- app/src/main/java/no/kristiania/pgr208_exam/fragments/UserPortfolioFragment.kt
+++ app/src/main/java/no/kristiania/pgr208_exam/fragments/UserPortfolioFragment.kt
@@ -9,17 +9,13 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import no.kristiania.pgr208_exam.PortfolioStatement
 import no.kristiania.pgr208_exam.R
 import no.kristiania.pgr208_exam.adapters.UserPortfolioAdapter
 import no.kristiania.pgr208_exam.data.API
 import no.kristiania.pgr208_exam.data.CoinCapService
-import no.kristiania.pgr208_exam.data.domain.CcOverview
-import no.kristiania.pgr208_exam.data.domain.SpecificCcData
 import no.kristiania.pgr208_exam.databinding.UserPortfolioFragmentBinding
 import no.kristiania.pgr208_exam.datastorage.db.DataBase
-import no.kristiania.pgr208_exam.datastorage.entities.UserPortfolio
 import no.kristiania.pgr208_exam.viewmodels.OverviewViewModel
 
 class UserPortfolioFragment : Fragment(R.layout.user_portfolio_fragment) {
@@ -31,8 +27,6 @@ class UserPortfolioFragment : Fragment(R.layout.user_portfolio_fragment) {
     val overviewModel = OverviewViewModel()
 
     private lateinit var adapter: UserPortfolioAdapter
-
-    private val coinCapService: CoinCapService = API.COIN_CAP_SERVICE
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -47,26 +41,23 @@ class UserPortfolioFragment : Fragment(R.layout.user_portfolio_fragment) {
         overviewModel.allCcAssets.observe(this, Observer {
             portfolios.clear()
             lifecycleScope.launch(Dispatchers.IO) {
-                var userPortfolios = DataBase.getDatabase(requireContext()).getUserPortfolioDAO().fetchAll()
-                userPortfolios.forEach {portfolio ->
-                    Log.d("INFO", "${portfolio.symbol} Volume: ${portfolio.volume}")
-                    userPortfolios.forEach {uPo ->
-                        val xyz = overviewModel.allCcAssets.value?.data?.find {it.symbol.equals(uPo.symbol)}
-                        if (xyz != null) {
-                            Log.d("INFO", "xyx values ${xyz.symbol}")
-                            portfolios.add(PortfolioStatement(uPo.symbol, uPo.volume, xyz.priceUsd.toString(), "${uPo.volume.toDouble() * (xyz.priceUsd!!.toDouble())}"))
-                        }
+                var userPortfolios =
+                    DataBase.getDatabase(requireContext()).getUserPortfolioDAO().fetchAll()
+                userPortfolios.forEach { portfolio ->
+                    val specificCcData =
+                        overviewModel.allCcAssets.value?.data?.find { it.symbol.equals(portfolio.symbol) }
+                    if (specificCcData != null) {
+                        Log.d("INFO", "xyx values ${specificCcData.symbol}")
+                        portfolios.add(
+                            PortfolioStatement(
+                                portfolio.symbol,
+                                portfolio.volume,
+                                specificCcData.priceUsd.toString(),
+                                "${portfolio.volume.toDouble() * (specificCcData.priceUsd!!.toDouble())}"
+                            )
+                        )
                     }
                 }
-                /*ccOverviews.clear()
-            ccOverviews.addAll(newCcData.data)
-            adapter.notifyDataSetChanged()*/
-
-                /*Log.d("STATEMENT", "${p.symbol} Volume: ${p.volume}")
-                    val overview = overviews[overviews.indexOfFirst { it.symbol == p.symbol}]
-                    Log.d("STATEMENT", "${overview.id} ${overview.symbol}")
-                    //val totalValue = p.volume.toDouble() * overview.priceUsd!!.toDouble()
-                    //testList.add(PortfolioStatement(p.symbol, p.volume, overview.priceUsd.toString(), "$totalValue"))*/
             }
             adapter.notifyDataSetChanged()
         })
